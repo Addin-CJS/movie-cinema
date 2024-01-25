d6f9206ceecda32bf3b335eb3ad2f22b7b51e15f
baeksejin <ssjin119@gmail.com>
2024-01-25 11:49:29 +0900

    회원가입 카카오 주소 안되는거 다시 처리, 이메일 아이디 주소 묶어서 DB에 저장

--- src/main/java/com/dealim/service/OpenApiService.java
+++ src/main/java/com/dealim/service/OpenApiService.java
@@ -4,7 +4,6 @@ import com.dealim.domain.Movie;
 import com.dealim.repository.MovieRepository;
 import info.movito.themoviedbapi.TmdbApi;
 import info.movito.themoviedbapi.TmdbMovies;
-import info.movito.themoviedbapi.model.Artwork;
 import info.movito.themoviedbapi.model.MovieDb;
 import info.movito.themoviedbapi.model.core.MovieResultsPage;
 import okhttp3.OkHttpClient;
@@ -13,7 +12,6 @@ import okhttp3.Response;
 import okhttp3.ResponseBody;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
