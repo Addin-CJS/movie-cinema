e010a09214f9e6ce1a690a3b158da75c4efa84d0
baeksejin <ssjin119@gmail.com>
2024-01-29 10:16:05 +0900

    ajax 삭제

--- src/main/java/com/dealim/controller/MovieController.java
+++ src/main/java/com/dealim/controller/MovieController.java
@@ -7,13 +7,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.text.DecimalFormat;
 import java.util.Optional;
@@ -99,22 +97,6 @@ public class MovieController {
             model.addAttribute("movie", null);
         }
         return "movie/movieSeats";
-    }
-
-    @GetMapping("/moviesList")
-    @ResponseBody
-    public ResponseEntity<Page<Movie>> getMovies(
-            @PageableDefault(page = 0, size = 8, sort = "movieId", direction = Sort.Direction.DESC) Pageable pageable,
-            @RequestParam(value = "searchKeyword", required = false) String searchKeyword) {
-
-        Page<Movie> movieList;
-        if (searchKeyword != null && !searchKeyword.trim().isEmpty()) {
-            movieList = movieService.findMoviesByKeyword(searchKeyword, pageable);
-        } else {
-            movieList = movieService.movieList(pageable);
-        }
-
-        return ResponseEntity.ok(movieList);
     }
 
     @GetMapping("/ticketing")
