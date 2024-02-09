8cc43838b883cfd81834ec1ca6199a04df10d27f
An <aaapple@naver.com>
2024-02-10 01:34:41 +0900

    feat : 영화1시간남았다는 알림 구현  ,SchedulerConfig 파일 생성 <scheduler패키지안에> , header.jsp안에 <jsp:include page="../movie/notification.jsp" />추가 사용자가 어떤페이지에 있든 알림받을수있게 알림 css는 아직 미완성

--- src/main/java/com/dealim/service/NotificationService.java
+++ src/main/java/com/dealim/service/NotificationService.java
@@ -1,14 +1,17 @@
 package com.dealim.service;
 
-import com.dealim.domain.Notification;
-import com.dealim.domain.Review;
+import com.dealim.domain.*;
+import com.dealim.repository.MemberRepository;
+import com.dealim.repository.MovieRepository;
 import com.dealim.repository.NotificationRepository;
 import com.dealim.repository.ReviewRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-
+import java.util.Optional;
+@Slf4j
 @Service
 public class NotificationService {
 
@@ -21,8 +24,16 @@ public class NotificationService {
     @Autowired
     private ReviewRepository reviewRepository;
 
+    @Autowired
+    private MovieRepository movieRepository;
+
+
+    @Autowired
+    private MemberRepository memberRepository;
+
     public void sendInterestMovieAddedNotification(String username, Long movieId) {
-        System.out.println("sendInterestMovieAddedNotification called with username: " + username + ", movieId: " + movieId); // 로그 추가
+        System.out.println("sendInterestMovieAddedNotification called with username: " + username + ", movieId: " + movieId);
+
         // 알림이 이미 존재하는지 확인 코드임
         if (!notificationRepository.existsByUsernameAndTypeAndMovieId(username, Notification.NotificationType.INTEREST_MOVIE_ADDED, movieId)) {
             // 새로운 알림 객체 생성 및 저장 코드임
@@ -36,10 +47,13 @@ public class NotificationService {
                     .build();
             Notification savedNotification = notificationRepository.save(notification);
 
-            // 고유 ID를 포함한 알림 JSON 생성는 코드임
-            String notificationJson = String.format("{\"id\": \"%d\", \"message\": \"안녕하세요, %s님. 관심 영화 '%d'가 추가되었습니다.\", \"type\": \"INTEREST_MOVIE_ADDED\"}", savedNotification.getId(), username, movieId);
+            // 알림 json 생성 코드임  키:값
+            String notificationJson = String.format("{\"id\": \"%d\", \"message\": \"안녕하세요, %s님. 관심 영화 '%d'가 추가되었습니다.\", \"type\": \"INTEREST_MOVIE_ADDED\"}",
+                                                    savedNotification.getId(),
+                                                    username,
+                                                    movieId);
             System.out.println("json~~~~~~"+notificationJson);
-            // SseEmitterService를 사용하여 특정 사용자에게 알림 전송 코드임
+            // 특정 사용자에게 알림 전송 코드임
             sseEmitterService.sendNotification(username, notificationJson);
 
         }
@@ -47,13 +61,13 @@ public class NotificationService {
     public void sendLikeNotification(Long reviewId, String likerUsername) {
         Review review = reviewRepository.findById(reviewId)
                 .orElseThrow(() -> new RuntimeException("리뷰를 찾을 수 없습니다."));
-        String ownerUsername = review.getReviewWriter(); // 리뷰 작성자의 사용자명
+        String  reviewWriterUsername = review.getReviewWriter(); // 리뷰 작성자의 사용자명
 
-        // "좋아요" 알림이 이미 존재하는지 확인
-        if (!notificationRepository.existsByUsernameAndTypeAndReviewId(ownerUsername, Notification.NotificationType.LIKE_NOTIFICATION, reviewId)) {
-            // 새로운 "좋아요" 알림 객체 생성 및 저장
+        // 좋아요 알림이 이미 존재하는지 확인 코드임
+        if (!notificationRepository.existsByUsernameAndTypeAndReviewId(reviewWriterUsername, Notification.NotificationType.LIKE_NOTIFICATION, reviewId)) {
+            // 새로운 좋아요 알림 객체 생성 및 저장 코드임
             Notification notification = Notification.builder()
-                    .username(ownerUsername)
+                    .username(reviewWriterUsername)
                     .type(Notification.NotificationType.LIKE_NOTIFICATION)
                     .reviewId(reviewId)
                     .isRead(false)
@@ -62,16 +76,64 @@ public class NotificationService {
                     .build();
             Notification savedNotification = notificationRepository.save(notification);
 
-            // 알림 JSON 생성
-            String notificationJson = String.format("{\"id\": \"%d\", \"message\": \"%s님이 당신의 리뷰 %d에 좋아요를 눌렀습니다.\", \"type\": \"LIKE_NOTIFICATION\"}", savedNotification.getId(), likerUsername, reviewId);
+            // 알림 json 생성 코드임  키:값
+            String notificationJson = String.format("{\"id\": \"%d\", \"message\": \"%s님이 당신의 리뷰 %d에 좋아요를 눌렀습니다.\", \"type\": \"LIKE_NOTIFICATION\"}",
+                                                savedNotification.getId(),
+                                                likerUsername,
+                                                reviewId);
 
-            // 특정 사용자에게 알림 전송
-            sseEmitterService.sendNotification(ownerUsername, notificationJson);
+            // 특정 사용자에게 알림 전송하는 코드 코드임
+            sseEmitterService.sendNotification(reviewWriterUsername, notificationJson);
         }
     }
 
+    public void sendMovieStartNotification(Long memberId, Ticket ticket) {
+        try {
+            // 영화 정보 조회
+            Optional<Movie> movieOpt = movieRepository.findById(ticket.getMovieId());
+            if (!movieOpt.isPresent()) {
+                log.error("영화 정보를 찾을 수 없습니다. Movie ID: {}", ticket.getMovieId());
+                return;
+            }
+            Movie movie = movieOpt.get();
 
+            // 회원 정보 조회 코드임
+            Optional<Member> memberOpt = memberRepository.findById(memberId);
+            if (!memberOpt.isPresent()) {
+                log.error("회원 정보를 찾을 수 없습니다. Member ID: {}", memberId);
+                return;
+            }
+            Member member = memberOpt.get();
 
+            // 알림 객체 생성 및 저장 코드임
+            Notification notification = Notification.builder()
+                    .username(member.getUsername())
+                    .type(Notification.NotificationType.MOVIE_BOOKED)
+                    .movieId(ticket.getMovieId())
+                    .isRead(false)
+                    .isSent(true)
+                    .createdDateTime(LocalDateTime.now())
+                    .build();
+            Notification savedNotification = notificationRepository.save(notification);
 
+            // 알림 json 생성 코드임  키:값
+            String notificationJson = String.format(
+                    "{\"id\": %d, \"message\": \"안녕하세요, 영화 '%s' 상영 시간이 한 시간 남았습니다! 상영시간: %s, 좌석: %s\", \"type\": \"MOVIE_BOOKED\"}",
+                    savedNotification.getId(),
+                    movie.getMvTitle(),
+                    ticket.getTicketedDate().toString(),
+                    ticket.getTicketedSeat());
 
+            log.info("영화 알림을 보냅니다 - {}", notificationJson);
+
+            //알림 전송하는 코드 코드임
+            sseEmitterService.sendNotification(member.getUsername(), notificationJson);
+        } catch (Exception e) {
+            log.error("영화 시작 알림 전송 중 오류 발생", e);
+        }
+    }
 }
+
+
+
+
